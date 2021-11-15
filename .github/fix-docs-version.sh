09c065006a682eb4b4a5508c0d067e996aad02ca
Mercedes Sabaj <mercedes.sabaj@abstracta.com>
2021-11-15 16:16:31 -0300

    Fix readme version update action job

--- .github/fix-docs-version.sh
+++ .github/fix-docs-version.sh
@@ -9,8 +9,7 @@ VERSION=$1
 update_file_versions() {
   local VERSION="$1"
   local FILE="$2"
-  sed -i "s/dm3270\(.*\):.*'/dm3270\1:${VERSION}'/g" "${FILE}"
-  sed -i "/dm3270*<\/artifactId>$/{N;s/dm3270\(.*\)<\/artifactId>\n  <version>.*<\/version>/dm3270\1<\/artifactId>\n  <version>${VERSION}<\/version>/}" "${FILE}"
+  sed -i "s/<version>.*<\/version>/<version>${VERSION}-lib<\/version>/g" README.md
 }
 
 update_file_versions ${VERSION} README.md
@@ -18,5 +17,5 @@ update_file_versions ${VERSION} README.md
 git add README.md
 git config --local user.email "$(git log --format='%ae' HEAD^!)"
 git config --local user.name "$(git log --format='%an' HEAD^!)"
-git commit -m "[skip ci] Updated docs artifacts versions"
+git commit -m "[skip ci] Updated README version"
 git push origin HEAD:master
